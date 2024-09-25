30480f9ad4edf41832dd42fe5f336c591ce7a99f
charlles69 <146854189+charlles69@users.noreply.github.com>
2024-09-25 08:13:16 -0300

    fiz o array e o for para os pacientes

--- Js/principal.js
+++ Js/principal.js
@@ -1,2 +1,13 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes=document.querySelectorAll(".paciente")
+for (var i= 0; 1 < pacientes.length; i++) {
+
+    var paciente = pacientes [i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent
+    var tdIMc = paciente.querySelector(".info-imc"); 
+}
